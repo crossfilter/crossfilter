4602446bebe065ef370c03e90c01392fc49af7a0
Jay Paulson <paulsjv@gmail.com>
2017-10-19 17:49:44 -0500

    first pass at TypeScript definitions file

---
+++ types/crossfilter2.d.ts
@@ -0,0 +1,82 @@
+declare namespace CrossFilter2 {
+
+    export interface Selector<T> {
+        (value: T): any;
+    }
+
+    export interface CrossFilterStatic {
+        <T>(data: T[]): CrossFilter<T>;
+        size(): number;
+        version: string;
+        permute<T>(arry: T[], index: number[], deep: number): T[];
+        heap: {
+            <T>(array: T[], lo: number, hi: number): T[];
+            by<T>(value: Selector<T>): Heap<T>;
+        }
+        heapselect: {
+            <T>(array: T[], lo: number, hi: number, k: number): T[];
+            by<T>(value: Selector<T>): HeapSelect<T>;
+        }
+        bisect: {
+            <T>(array: T[], value: T, lo: number, hi: number): number;
+            by<T>(value: Selector<T>): Bisector<T>;
+        }
+        insertionsort: {
+            <T>(array: T[], lo: number, hi: number): T[];
+            by<T>(vaule: Selector<T>): Sort<T>;
+        }
+        quicksort: {
+            <T>(array: T[], lo: number, hi: number): T[];
+            by<T>(value: Selector<T>): Sort<T>;
+        }
+    }
+
+
+    export interface CrossFilter<T> {
+        add(records: T[]): CrossFilter<T>;
+        remove(predicate?: Function): void;
+        dimension<T>(value: Function, isArray?: boolean): Dimension<T>;
+        groupAll(): any; // return vale?
+        size(): number;
+        all(): Array<T>;
+        allFiltered(): Array<T>;
+        onChange(callback: Function): Function;
+        isElementFiltered(index: number, ignoreDimensions?: number): boolean;
+
+    }
+
+    export interface Dimension<T> {
+        filter(range: any): any; // returns Array?
+        filterExact(value: any): any; // value most likely either string or number? Returns Array?
+        filterRange(range: Array<T>): any; // returns Array?
+        filterFunction(funct: Function): any;
+        filterAll(): any; // returns Array?
+        top(k: number, offset?: number): Array<T>;
+        bottom(k: number, offset?: number): Array<T>;
+        group(groupValue: Function): Group<T>;
+        groupAll(): any;
+        dispose(): Array<T>;
+        remove(): Array<T>;
+        accessor: any;
+        id: Function;
+    }
+
+    export interface Group<T> {
+        top(k: number): Array<T>;
+        all(): Array<Group<T>>; // return groups?
+        reduce(add: any, remove: any, initial: any): Group<T>;
+        reduceCount(): Group<T>;
+        reduceSum(func: Function): Group<T>;
+        order(orderValue: Function): Group<T>;
+        orderNatural(): Group<T>;
+        size(): number;
+        dispose(): Group<T>;
+        remove(): Group<T>;
+
+    }
+}
+declare var crossfilter: CrossFilter2.CrossFilterStatic;
+declare module "crossfilter2" {
+    var crossfilter: CrossFilter2.CrossFilterStatic;
+    export = crossfilter;
+}
