2858485c72c82d8446113db76c313fac5720d8be
Gordon Woodhull <gordon@woodhull.com>
2019-05-30 12:47:14 -0400

    restored typed arrays that were disabled in 70fb3dca54 / db6ff2d1f8a

--- src/array.js
+++ src/array.js
@@ -1,16 +1,21 @@
+var crossfilter_array8 = crossfilter_arrayUntyped,
+    crossfilter_array16 = crossfilter_arrayUntyped,
+    crossfilter_array32 = crossfilter_arrayUntyped,
+    crossfilter_arrayLengthen = crossfilter_arrayLengthenUntyped,
+    crossfilter_arrayWiden = crossfilter_arrayWidenUntyped;
 if (typeof Uint8Array !== "undefined") {
-  var crossfilter_array8 = function(n) { return new Uint8Array(n); };
-  var crossfilter_array16 = function(n) { return new Uint16Array(n); };
-  var crossfilter_array32 = function(n) { return new Uint32Array(n); };
+  crossfilter_array8 = function(n) { return new Uint8Array(n); };
+  crossfilter_array16 = function(n) { return new Uint16Array(n); };
+  crossfilter_array32 = function(n) { return new Uint32Array(n); };
 
-  var crossfilter_arrayLengthen = function(array, length) {
+  crossfilter_arrayLengthen = function(array, length) {
     if (array.length >= length) return array;
     var copy = new array.constructor(length);
     copy.set(array);
     return copy;
   };
 
-  var crossfilter_arrayWiden = function(array, width) {
+  crossfilter_arrayWiden = function(array, width) {
     var copy;
     switch (width) {
       case 16: copy = crossfilter_array16(array.length); break;
@@ -177,10 +182,10 @@ crossfilter_bitarray.prototype.onlyExcept = function(n, offset, zero, onlyOffset
 };
 
 module.exports = {
-  array8: crossfilter_arrayUntyped,
-  array16: crossfilter_arrayUntyped,
-  array32: crossfilter_arrayUntyped,
-  arrayLengthen: crossfilter_arrayLengthenUntyped,
-  arrayWiden: crossfilter_arrayWidenUntyped,
+  array8: crossfilter_array8,
+  array16: crossfilter_array16,
+  array32: crossfilter_array32,
+  arrayLengthen: crossfilter_arrayLengthen,
+  arrayWiden: crossfilter_arrayWiden,
   bitarray: crossfilter_bitarray
 };
