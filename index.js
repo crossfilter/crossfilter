6198988c7729f21268a1db6c9bf03463cb342dda
christophe-g <christophe.geiser@gmail.com>
2019-10-17 14:43:54 +0200

    exporting current version for umd build

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-export { default } from  './src/index.js';
+// Note(cg): exporting current version for umd build.
+import crossfilter from './src/index.js';
+import { version } from './package.json';
+crossfilter.version = version;
+
+export default crossfilter;
